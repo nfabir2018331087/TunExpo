fd59da698cb356fd6c336d329e4f2ffb2aac3d4d
IT Gadget <admin@ITs-MacBook-Pro.local>
2023-02-01 01:17:52 +0600

    added some code

--- src/Components/Hero/Hero.js
+++ src/Components/Hero/Hero.js
@@ -7,9 +7,9 @@ export default function Hero() {
     <div className='hero-bg'>
     <div className='d-flex container'>
     <div className='w-50 text-start text-white pt-xl'>
-        <h1>Capture Your face and show <br/> whats on your mind</h1>
-        <p className='my-4 text-white'>Is artificial intelligence less than our intelligence ?</p>
-        <button className='btn-primary btn px-5 py-3 rounded-pill fs-5'> <img src={icon} className="me-2 ms-0" width="45px" height="40px" alt=""/>Explore TuneExpo</button>
+        <h1 className='fontHero'>Capture Your face and show <br/> whats on your mind</h1>
+        <p className='my-4  fontP text-white'>Artificial intelligence  is not less than our intelligence </p>
+        <button className='btn-primary btn px-5 py-2 rounded-3 fs-5'> <img src={icon} className="me-2 ms-0" width="45px" height="40px" alt=""/>Explore TuneExpo</button>
         
      </div>
      <div className='w-50'>
